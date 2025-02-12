46328d356530a84075925122a2cb8808a1287966
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-02-12 09:07:10 +0100

    final version N1Ex2; a bit more N2Ex1

--- N1/Ex2.sql
+++ N1/Ex2.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2025-02-11 12:42
+-- Generated: 2025-02-11 17:24
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`costumers` (
   `costumers_zipcode` INT(6) NULL DEFAULT NULL,
   `localities_localities_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`costumers_id`),
-  INDEX `fk_costumers_localities1_idx` (`localities_localities_id` ASC),
+  INDEX `fk_costumers_localities1_idx` (`localities_localities_id` ASC) VISIBLE,
   CONSTRAINT `fk_costumers_localities1`
     FOREIGN KEY (`localities_localities_id`)
     REFERENCES `Pizzeria`.`localities` (`localities_id`)
@@ -34,11 +34,18 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`orders` (
   `orders_totalPrice` FLOAT(6) NOT NULL,
   `orders_type` ENUM("Delivery", "Pick up") NOT NULL,
   `employees_employees_id` INT(11) NULL DEFAULT NULL,
+  `costumers_costumers_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`orders_id`),
-  INDEX `fk_orders_employees1_idx` (`employees_employees_id` ASC),
+  INDEX `fk_orders_employees1_idx` (`employees_employees_id` ASC) VISIBLE,
+  INDEX `fk_orders_costumers1_idx` (`costumers_costumers_id` ASC) VISIBLE,
   CONSTRAINT `fk_orders_employees1`
     FOREIGN KEY (`employees_employees_id`)
     REFERENCES `Pizzeria`.`employees` (`employees_id`)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_orders_costumers1`
+    FOREIGN KEY (`costumers_costumers_id`)
+    REFERENCES `Pizzeria`.`costumers` (`costumers_id`)
     ON DELETE SET NULL
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -47,18 +54,18 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `Pizzeria`.`products` (
   `products_id` INT(11) NOT NULL AUTO_INCREMENT,
   `products_name` VARCHAR(45) NOT NULL,
-  `products_type` ENUM("Pizza", "Burger", "Drink") NOT NULL,
+  `products_type` ENUM("Pizza", "Burger", "Drink") NULL DEFAULT NULL,
   `products_price` FLOAT(5) NOT NULL,
   `products_img` BLOB NULL DEFAULT NULL,
   `pizza_categories_pizza_categories_id` INT(11) NULL DEFAULT NULL,
   `orders_orders_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`products_id`),
-  INDEX `fk_products_pizza_categories1_idx` (`pizza_categories_pizza_categories_id` ASC),
-  INDEX `fk_products_orders1_idx` (`orders_orders_id` ASC),
+  INDEX `fk_products_pizza_categories1_idx` (`pizza_categories_pizza_categories_id` ASC) VISIBLE,
+  INDEX `fk_products_orders1_idx` (`orders_orders_id` ASC) VISIBLE,
   CONSTRAINT `fk_products_pizza_categories1`
     FOREIGN KEY (`pizza_categories_pizza_categories_id`)
     REFERENCES `Pizzeria`.`pizza_categories` (`pizza_categories_id`)
-    ON DELETE CASCADE
+    ON DELETE SET NULL
     ON UPDATE CASCADE,
   CONSTRAINT `fk_products_orders1`
     FOREIGN KEY (`orders_orders_id`)
@@ -80,13 +87,13 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`stores` (
   `stores_id` INT(11) NOT NULL AUTO_INCREMENT,
   `stores_address` VARCHAR(45) NULL DEFAULT NULL,
   `stores_zipcode` VARCHAR(45) NULL DEFAULT NULL,
-  `localities_localities_id` INT(11) NOT NULL,
+  `localities_localities_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`stores_id`),
-  INDEX `fk_stores_localities1_idx` (`localities_localities_id` ASC),
+  INDEX `fk_stores_localities1_idx` (`localities_localities_id` ASC) VISIBLE,
   CONSTRAINT `fk_stores_localities1`
     FOREIGN KEY (`localities_localities_id`)
     REFERENCES `Pizzeria`.`localities` (`localities_id`)
-    ON DELETE CASCADE
+    ON DELETE SET NULL
     ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
@@ -98,13 +105,13 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`employees` (
   `employees_nif` VARCHAR(45) NOT NULL,
   `employees_tel` INT(12) NOT NULL,
   `employees_role` ENUM("Delivery", "Kitchen") NOT NULL,
-  `stores_stores_id` INT(11) NOT NULL,
+  `stores_stores_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`employees_id`),
-  INDEX `fk_employees_stores1_idx` (`stores_stores_id` ASC),
+  INDEX `fk_employees_stores1_idx` (`stores_stores_id` ASC) VISIBLE,
   CONSTRAINT `fk_employees_stores1`
     FOREIGN KEY (`stores_stores_id`)
     REFERENCES `Pizzeria`.`stores` (`stores_id`)
-    ON DELETE CASCADE
+    ON DELETE SET NULL
     ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
@@ -114,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`localities` (
   `localities_name` VARCHAR(45) NOT NULL,
   `provinces_provinces_id` INT(11) NOT NULL,
   PRIMARY KEY (`localities_id`),
-  INDEX `fk_localities_provinces_idx` (`provinces_provinces_id` ASC),
+  INDEX `fk_localities_provinces_idx` (`provinces_provinces_id` ASC) VISIBLE,
   CONSTRAINT `fk_localities_provinces`
     FOREIGN KEY (`provinces_provinces_id`)
     REFERENCES `Pizzeria`.`provinces` (`provinces_id`)
@@ -127,6 +134,19 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`provinces` (
   `provinces_id` INT(11) NOT NULL AUTO_INCREMENT,
   `provinces_name` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`provinces_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `Pizzeria`.`orders_has_productType` (
+  `orders_orders_id` INT(11) NOT NULL,
+  `orders_productType` ENUM("Pizza", "Burger", "Drink") NOT NULL,
+  `orders_has_productType_num` INT(5) NOT NULL,
+  PRIMARY KEY (`orders_orders_id`, `orders_productType`),
+  CONSTRAINT `fk_orders_has_productType_orders1`
+    FOREIGN KEY (`orders_orders_id`)
+    REFERENCES `Pizzeria`.`orders` (`orders_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
